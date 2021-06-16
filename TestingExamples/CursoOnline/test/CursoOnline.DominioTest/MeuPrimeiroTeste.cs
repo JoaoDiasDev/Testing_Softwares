6da15351fa8b13b5b4d757385b537cef33cf06e6
JoaoDiasDev <79063684+JoaoDiasDev@users.noreply.github.com>
2021-06-15 23:41:46 -0300

    Studying Tests

---
+++ TestingExamples/CursoOnline/test/CursoOnline.DominioTest/MeuPrimeiroTeste.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CursoOnline.DominioTest
+{
+    public class MeuPrimeiroTeste
+    {
+        [Fact(DisplayName = "DeveAsVariaveisTeremOMesmoValor")]
+        public void DeveAsVariaveisTeremOMesmoValor()
+        {
+            //Three steps = Organize, Act, Assert
+
+            //Organização
+            var variavel1 = 1;
+            var variavel2 = 1;
+
+            //Ação
+            variavel2 = variavel1;
+            
+            //Assert
+            Assert.Equal(variavel1, variavel2);
+            //Assert.True(variavel1 == variavel2);
+            //Assert.False(variavel1 == variavel2);
+
+        }
+    }
+}
