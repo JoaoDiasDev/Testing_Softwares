9293822f9e3d8f40fbca01015417fbec476a94f3
JoaoDiasDev <79063684+JoaoDiasDev@users.noreply.github.com>
2021-06-17 23:01:28 -0300

    Testing Std.

---
+++ TestingExamples/CursoOnline/test/CursoOnline.DominioTest/Cursos/Curso.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    internal class Curso
+    {
+   
+
+        public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
+        {
+            if (string.IsNullOrEmpty(nome))
+            {
+                throw new ArgumentException("Nome Inválido");
+            }
+
+            if (cargaHoraria < 1)
+            {
+                throw new ArgumentException("Carga horária inválida");
+            }
+
+            if (valor < 1)
+            {
+                throw new ArgumentException("Valor inválido");
+            }
+
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+        }
+
+        public string Nome { get; internal set; }
+        public double CargaHoraria { get; internal set; }
+        public double Valor { get; internal set; }
+        public PublicoAlvo PublicoAlvo { get; internal set; }
+    }
+}
---
+++ TestingExamples/CursoOnline/test/CursoOnline.DominioTest/Cursos/CursoTest.cs
@@ -0,0 +1,95 @@
+﻿using ExpectedObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class CursoTest
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            var cursoEsperado = new
+            {
+                Nome = "Informática básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950
+            };
+
+            var curso = new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor);
+
+            cursoEsperado.ToExpectedObject().ShouldMatch(curso);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void NaoDeveCursoTerUmNomeInvalido(string nomeInvalido)
+        {
+
+            var cursoEsperado = new
+            {
+                Nome = "Informática básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950
+            };
+
+            string message = Assert.Throws<ArgumentException>(() => new Curso(nomeInvalido, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor)).Message;
+            Assert.Equal("Nome Inválido", message);
+
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-100)]
+        public void NaoDeveCursoTerUmaCargaHorariaMenorQue1(double cargaHorariaInvalida)
+        {
+
+            var cursoEsperado = new
+            {
+                Nome = "Informática básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950
+            };
+
+            var message = Assert.Throws<ArgumentException>(() => new Curso(cursoEsperado.Nome, cargaHorariaInvalida, cursoEsperado.PublicoAlvo, cursoEsperado.Valor)).Message;
+            Assert.Equal("Carga horária inválida", message);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-100)]
+        public void NaoDeveCursoTerUmValorMenorQue1(double valorInvalido)
+        {
+
+            var cursoEsperado = new
+            {
+                Nome = "Informática básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = (double)950
+            };
+
+            var message = Assert.Throws<ArgumentException>(() => new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, valorInvalido)).Message;
+                Assert.Equal("Valor inválido", message);
+        }
+
+    }
+
+    public enum PublicoAlvo
+    {
+        Estudante,
+        Universitário,
+        Empregado,
+        Empreendedor
+    }
+}
